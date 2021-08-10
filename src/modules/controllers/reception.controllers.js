f74530eef9e0caf5cd9752afcae3ad3902e86fa2
user <rallakhverdiev.exceedteam@gmail.com>
2021-08-10 17:21:10 +0300

    fix mistakes

--- src/modules/controllers/reception.controllers.js
+++ src/modules/controllers/reception.controllers.js
@@ -1,27 +1,41 @@
 const Receptions = require('../../db/models/receptions/Index');
+const jwt = require('jsonwebtoken')
+
+SECRET_KEY = 'Rustam4ik228322';
+const tokenVerify = (token) => {
+  const decoded = jwt.verify(token, SECRET_KEY);
+  return decoded;
+}
 
 module.exports.getAllReceptions = (req, res) => {
-  Receptions.find({ idUser: req.query.idUser }).then(result => {
+  const tokenParse = tokenVerify(req.headers.token);
+
+  Receptions.find({ idUser: tokenParse._id }, ['nameUser', 'nameDoctor', 'date', 'complaint']).then(result => {
     res.send({ data: result });
   });
 };
 
 module.exports.createNewReception = (req, res) => {
-  const reception = new Receptions(req.body);
+  const tokenParse = tokenVerify(req.headers.token);
+
+  req.body["idUser"] = tokenParse._id;
+
   const body = req.body;
 
-  if (body.hasOwnProperty('nameUser') 
-  && body.hasOwnProperty('nameDoctor') 
-  && body.hasOwnProperty('date')
-  && body.hasOwnProperty('complaint')
-  && body.hasOwnProperty('idUser')
-  && body.nameUser
-  && body.nameDoctor
-  && body.date
-  && body.complaint
-  && body.idUser) {
+  const reception = new Receptions(body);
+
+  if (body.hasOwnProperty('nameUser')
+    && body.hasOwnProperty('nameDoctor')
+    && body.hasOwnProperty('date')
+    && body.hasOwnProperty('complaint')
+    && body.hasOwnProperty('idUser')
+    && body.nameUser
+    && body.nameDoctor
+    && body.date
+    && body.complaint
+    && body.idUser) {
     reception.save(body).then(() => {
-      Receptions.find({ idUser: body.idUser }).then(result => {
+      Receptions.find({ idUser: tokenParse._id }, ['nameUser', 'nameDoctor', 'date', 'complaint']).then(result => {
         res.send({ data: result });
       });
     });
@@ -32,13 +46,16 @@ module.exports.createNewReception = (req, res) => {
 
 module.exports.editReception = (req, res) => {
   const body = req.body;
+  const headers = req.headers;
 
   if (body.hasOwnProperty('_id')
-  && body.hasOwnProperty('idUser')
-  && body._id
-  && body.idUser) {
-    Receptions.updateOne({ _id: req.body._id }, req.body).then(() => {
-      Receptions.find({ idUser: req.body.idUser }).then(result => {
+    && headers.hasOwnProperty('token')
+    && body._id
+    && headers.token) {
+    const tokenParse = tokenVerify(headers.token);
+
+    Receptions.updateOne({ _id: body._id }, body).then(() => {
+      Receptions.find({ idUser: tokenParse._id }, ['nameUser', 'nameDoctor', 'date', 'complaint']).then(result => {
         res.send({ data: result });
       });
     });
@@ -48,14 +65,17 @@ module.exports.editReception = (req, res) => {
 };
 
 module.exports.deleteReception = async (req, res) => {
-  const query = req.query;
-  if (query.hasOwnProperty('_id')
-  && query.hasOwnProperty('idUser')
-  && query._id
-  && query.idUser) {
-    Receptions.deleteOne({ _id: query._id }).then(() => {
-      Receptions.find({ idUser: query.idUser }).then(result => {
-        res.send({data: result});
+  const header = req.headers;
+
+  if (header.hasOwnProperty('_id')
+    && header.hasOwnProperty('token')
+    && header._id
+    && header.token) {
+    const tokenParse = tokenVerify(header.token);
+
+    Receptions.deleteOne({ _id: header._id }).then(() => {
+      Receptions.find({ idUser: tokenParse._id }, ['nameUser', 'nameDoctor', 'date', 'complaint']).then(result => {
+        res.send({ data: result });
       });
     });
   } else {
