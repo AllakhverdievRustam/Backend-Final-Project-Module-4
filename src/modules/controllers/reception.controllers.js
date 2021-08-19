bfe6a61790b136fda0a00a6ead844977eabefde1
user <rallakhverdiev.exceedteam@gmail.com>
2021-08-19 18:12:49 +0300

    edit requests

--- src/modules/controllers/reception.controllers.js
+++ src/modules/controllers/reception.controllers.js
@@ -1,5 +1,6 @@
 const Receptions = require('../../db/models/receptions/Index');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const { query } = require('express');
 
 SECRET_KEY = 'Rustam4ik228322';
 const tokenVerify = (token) => {
@@ -59,12 +60,21 @@ module.exports.getAllReceptions = (req, res) => {
 
 module.exports.createNewReception = (req, res) => {
   const { body, headers } = req;
+  const { limit, offset, sortLable, sortDirection, firstDate, lastDate } = body;
 
   const flag = body.hasOwnProperty('nameUser')
     && body.hasOwnProperty('nameDoctor')
     && body.hasOwnProperty('date')
     && body.hasOwnProperty('complaint')
+    && body.hasOwnProperty('limit')
+    && body.hasOwnProperty('offset')
     && headers.hasOwnProperty('authorization')
+    && body.hasOwnProperty('sortLable')
+    && body.hasOwnProperty('sortDirection')
+    && body.hasOwnProperty('firstDate')
+    && body.hasOwnProperty('lastDate')
+    && limit !== ''
+    && offset !== ''
     && body.nameUser
     && body.nameDoctor
     && body.date
@@ -74,15 +84,38 @@ module.exports.createNewReception = (req, res) => {
   if (flag) {
     const tokenParse = tokenVerify(headers.authorization);
 
+    const sortArr = [];
+    (sortLable && sortDirection)
+      ? sortArr.push({ [sortLable]: sortDirection === "asc" ? 1 : -1 })
+      : sortArr.push({ ["_id"]: 1 })
+
+    const filterArr = [{ idUser: tokenParse._id }];
+    if (firstDate && lastDate) {
+      filterArr.push({ date: { $gte: firstDate, $lte: lastDate } });
+    } else if (firstDate && !lastDate) {
+      filterArr.push({ date: { $gte: firstDate } });
+    } else if (!firstDate && lastDate) {
+      filterArr.push({ date: { $lte: lastDate } });
+    }
+
+    const limitNun = +(limit);
+    const startInd = +(offset) * limitNun;
+
     body["idUser"] = tokenParse._id;
     const reception = new Receptions(body);
 
     reception.save(body).then(() => {
       Receptions.find(
-        { idUser: tokenParse._id },
+        { $and: filterArr },
         ['nameUser', 'nameDoctor', 'date', 'complaint'])
+        .sort(sortArr[0])
+        .skip(startInd).limit(limitNun)
         .then(result => {
-          res.send({ data: result });
+          Receptions.count(
+            { $and: filterArr })
+            .then(resultCount => {
+              res.send({ data: result, length: resultCount });
+            });
         });
     });
   } else {
@@ -92,19 +125,51 @@ module.exports.createNewReception = (req, res) => {
 
 module.exports.editReception = (req, res) => {
   const { body, headers } = req;
+  const { limit, offset, sortLable, sortDirection, firstDate, lastDate } = body;
 
   if (body.hasOwnProperty('_id')
+    && body.hasOwnProperty('limit')
+    && body.hasOwnProperty('offset')
     && headers.hasOwnProperty('authorization')
+    && body.hasOwnProperty('sortLable')
+    && body.hasOwnProperty('sortDirection')
+    && body.hasOwnProperty('firstDate')
+    && body.hasOwnProperty('lastDate')
+    && limit !== ''
+    && offset !== ''
     && body._id
     && headers.authorization) {
     const tokenParse = tokenVerify(headers.authorization);
 
+    const sortArr = [];
+    (sortLable && sortDirection)
+      ? sortArr.push({ [sortLable]: sortDirection === "asc" ? 1 : -1 })
+      : sortArr.push({ ["_id"]: 1 })
+
+    const filterArr = [{ idUser: tokenParse._id }];
+    if (firstDate && lastDate) {
+      filterArr.push({ date: { $gte: firstDate, $lte: lastDate } });
+    } else if (firstDate && !lastDate) {
+      filterArr.push({ date: { $gte: firstDate } });
+    } else if (!firstDate && lastDate) {
+      filterArr.push({ date: { $lte: lastDate } });
+    }
+
+    const limitNun = +(limit);
+    const startInd = +(offset) * limitNun;
+
     Receptions.updateOne({ _id: body._id }, body).then(() => {
       Receptions.find(
-        { idUser: tokenParse._id },
+        { $and: filterArr },
         ['nameUser', 'nameDoctor', 'date', 'complaint'])
+        .sort(sortArr[0])
+        .skip(startInd).limit(limitNun)
         .then(result => {
-          res.send({ data: result });
+          Receptions.count(
+            { $and: filterArr })
+            .then(resultCount => {
+              res.send({ data: result, length: resultCount });
+            });
         });
     });
   } else {
@@ -114,19 +179,53 @@ module.exports.editReception = (req, res) => {
 
 module.exports.deleteReception = async (req, res) => {
   const { headers, query } = req;
+  const { limit, offset, sortLable, sortDirection, firstDate, lastDate, _id } = query;
 
   if (query.hasOwnProperty('_id')
+    && query.hasOwnProperty('limit')
+    && query.hasOwnProperty('offset')
     && headers.hasOwnProperty('authorization')
-    && query._id
-    && headers.authorization) {
+    && query.hasOwnProperty('sortLable')
+    && query.hasOwnProperty('sortDirection')
+    && query.hasOwnProperty('firstDate')
+    && query.hasOwnProperty('lastDate')
+    && limit !== ''
+    && offset !== ''
+    && _id
+    && headers.authorization
+    ) {
+    
     const tokenParse = tokenVerify(headers.authorization);
 
-    Receptions.deleteOne({ _id: query._id }).then(() => {
+    const sortArr = [];
+    (sortLable && sortDirection)
+      ? sortArr.push({ [sortLable]: sortDirection === "asc" ? 1 : -1 })
+      : sortArr.push({ ["_id"]: 1 })
+
+    const filterArr = [{ idUser: tokenParse._id }];
+    if (firstDate && lastDate) {
+      filterArr.push({ date: { $gte: firstDate, $lte: lastDate } });
+    } else if (firstDate && !lastDate) {
+      filterArr.push({ date: { $gte: firstDate } });
+    } else if (!firstDate && lastDate) {
+      filterArr.push({ date: { $lte: lastDate } });
+    }
+
+    const limitNun = +(limit);
+    const startInd = +(offset) * limitNun;
+
+    Receptions.deleteOne({ _id }).then(() => {
       Receptions.find(
-        { idUser: tokenParse._id },
+        { $and: filterArr },
         ['nameUser', 'nameDoctor', 'date', 'complaint'])
+        .sort(sortArr[0])
+        .skip(startInd).limit(limitNun)
         .then(result => {
-          res.send({ data: result });
+          Receptions.count(
+            { $and: filterArr })
+            .then(resultCount => {
+              res.send({ data: result, length: resultCount });
+            });
         });
     });
   } else {
